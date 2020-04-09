665d1ce0d8db1fca9cf4974a5899bea7d3d0ee29
Alius <alexa.glinsoo@gmail.com>
2020-04-09 18:58:52 -0300

    pegando nome do pokemon

--- main.py
+++ main.py
@@ -1,35 +1,68 @@
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler)
+import requests
+from telegram.ext import (Updater, CommandHandler, MessageHandler,
+                          Filters, RegexHandler, ConversationHandler)
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 path = 'https://pokeapi.co/api/v2/'
+STATE1 = 1
+STATE2 = 2
 
-#mensagem de inicio
+# mensagem de inicio
+
+
 def start(update, context):
-    message = """Olá, treinador! 
-                        \nDitige o nome ou o cógido do pokémon: """
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    try:
+        message = """Olá, treinador! 
+                            \nDitige o nome ou o cógido do pokémon: """
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        update.message.reply_text(
+            message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True))
+        return STATE1
+    except Exception as e:
+        print(str(e))
 
 
-#cancelando bot e dando tchau
+def isPokemon(update, context):
+    pokemon = update.message.text
+    print(pokemon)
+    if len(pokemon) < 1:
+        message = """Dado inválido. Por favor, ditige o nome ou o cógido do pokémon: """
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text=message)
+        return STATE1
+    else:
+        return STATE2
+
+
+def searchPokemon(update, context):
+    pokemon = update.message.text
+
+# cancelando bot e dando tchau
 def close(update, context):
     message = "Tchau, treinador!"
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     return ConversationHandler.END
 
-#main
+# main
 def main():
-    #config
+   
+    myToken = ''
+    # config
     updater = Updater(token=myToken, use_context=True)
     updater.dispatcher.add_handler(CommandHandler('start', start))
-    #opções de conversas
+    # opções de conversas
     conversation_handler = ConversationHandler(
         entry_points=[
             CommandHandler('start', start),
             CommandHandler('close', close)
         ],
+        states={
+            STATE1: [MessageHandler(Filters.text, isPokemon)],
+            STATE2: [MessageHandler(Filters.text, searchPokemon)],
+        },
         fallbacks=[CommandHandler('close', close)])
 
-    #iniciando
+    # iniciando
     updater.dispatcher.add_handler(conversation_handler)
     updater.start_polling()
     updater.idle()
