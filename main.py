2e1863a855fd3a77eb0b9345b1e49c895391ceb2
Alius <alexa.glinsoo@gmail.com>
2020-04-09 23:34:45 -0300

    validando data

--- main.py
+++ main.py
@@ -7,32 +7,55 @@ path = 'https://pokeapi.co/api/v2/pokemon/'
 STATE1 = 1
 
 # mensagem de inicio
+
+
 def start(update, context):
-    message = """Olá, treinador! 
+    message = """Olá, treinador!
                         \nDigite /pokemon para buscar"""
-    update.message.reply_text(
-        message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
 
 def isPokemon(update, context):
     message = 'Digite o nome ou o código do pokémon:'
-    update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True))
+    update.message.reply_text(
+        message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True))
     return STATE1
+
 
 def searchPokemon(update, context):
     pokemon = update.message.text
-    print(path + pokemon)
     data = requests.get(path + pokemon)
-    print(data.json())
+    if data.status_code != 200:
+        message = """Dado inválido
+                        \nDigite o nome ou o código do pokémon:"""
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    else:
+        dataJson = data.json()
+
+        id = dataJson["id"]
+        name = dataJson["name"]
+        type = dataJson["types""type"]
+        weight = dataJson["weight"]
+        height = dataJson["height"]
+        print(type)
+        #message =  'Id: ' + id + '\nNome: ' + name +'\nTipo: ' + type +'\nPeso: ' + weight +'\nAltura: ' + height +'\n'
+        #context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return ConversationHandler.END
 
 # cancelando bot e dando tchau
+
+
 def close(update, context):
     message = "Tchau, treinador!"
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
     return ConversationHandler.END
 
 # main
+
+
 def main():
-    myToken = ''
+   
+     myToken = ''
     # config
     updater = Updater(token=myToken, use_context=True)
     updater.dispatcher.add_handler(CommandHandler('start', start))
---
+++ pokemon.py
@@ -0,0 +1,7 @@
+class Pokemon:
+    def __init__(self, id, name, type, weight, height):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.weight = weight
+        self.height = height
